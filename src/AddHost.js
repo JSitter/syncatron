594764735346b2e9fdd592d2b50e5116237d1517
Justin Sitter <justin@jaytria.com>
2020-08-13 23:10:59 -0700

    Add host input components

---
+++ src/AddHost.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import './AddHost.css';
+
+export default function AddHost(props) {
+    const [ hostName, setHostName ] = useState('');
+    const [ userName, setUserName ] = useState('');
+    const [ password, setPassword ] = useState('');
+    const [ showPassword, setShowPassword ] = useState('password')
+
+    function toggleShowPassword() {
+        if( showPassword === 'password'){
+            setShowPassword('text');
+        }else{
+            setShowPassword('password');
+        }
+    }
+
+    function formSubmission(e) {
+        e.preventDefault()
+        props.addNewHost(hostName, userName, password);
+    }
+
+    return (
+        <form>
+            <label htmlFor='hostname'>Host Name
+                <input 
+                    type='text' 
+                    name='hostname' 
+                    id='hostname' 
+                    onChange={(e)=>setHostName(e.target.value)} 
+                    value={hostName}>
+                </input>
+            </label>
+            <label htmlFor='username'>User Name
+                <input 
+                    type='text' 
+                    name='username' 
+                    id='username' 
+                    onChange={(e)=>setUserName(e.target.value)} 
+                    value={userName}>
+                </input>
+            </label>
+            <label htmlFor='password'>Password
+                <input 
+                    type={showPassword} 
+                    name='password' 
+                    id='password' 
+                    onChange={(e) => setPassword(e.target.value)} 
+                    value={password}>
+                </input>
+            </label>
+            <p onClick={toggleShowPassword}>Show Password</p>
+            <button type='submit' onClick={formSubmission}>Save Host</button>
+            <button type='button' onClick={props.cancel}>Cancel</button>
+        </form>
+
+    )
+};
---
+++ src/SavedHosts.js
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from 'react';
+import './AddHost.js';
+import './SavedHosts.css';
+import AddHost from './AddHost';
+const { ipcRenderer } = window.require('electron');
+
+function Host(hostname) {
+    return (
+        <div>
+            <p>{hostname}</p>
+        </div>
+    )
+}
+
+function displayHosts(hosts) {
+    let hostComponents = [];
+    for( let i = 0; i < hosts.length; i++) {
+        hostComponents.push(Host(hosts[i]));
+    }
+    if(hostComponents.length === 0){
+        return "No Saved Hosts"
+    }
+    return hostComponents;
+}
+
+function addNewHost(hostname, username, password) {
+    console.log(hostname, username, password);
+}
+
+
+
+export default function SavedHosts() {
+    const [ hosts, setHosts ] = useState([]);
+    const [ addHost, setAddHost ] = useState(false);
+
+    function cancelSubmission() {
+        setAddHost(false);
+    }
+
+    useEffect(()=>{
+        ipcRenderer.send('get-hosts')
+    }, [hosts]);
+    ipcRenderer.on('host-list', (event, serverList) => {
+        console.log(serverList);
+    })
+    return (
+        <section>
+            <h1>Hosts</h1>
+            { addHost ? <AddHost 
+                            cancel={cancelSubmission}
+                            addNewHost={addNewHost}
+                        /> : <p onClick={()=>{setAddHost(true)}}>
+                                + Add New Host
+                            </p> }
+            { displayHosts(hosts) }
+        </section>
+    )
+}
